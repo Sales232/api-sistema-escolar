09d2b77ab45aa8dc1240e52c1052d493f74ad8fd
Pedro Henrique <129916174+Sales232@users.noreply.github.com>
2025-06-15 19:26:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ app.include_router(turmas.router)
 app.include_router(init.router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 professores = carregar_dados('professores.json')
 alunos = carregar_dados('alunos.json')
